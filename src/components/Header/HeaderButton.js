43528ad26513679819f4c375f84d14fbc18d5991
Rohan Joby <rohanjobyak@gmail.com>
2021-06-08 14:54:41 +0530

    wip: add logic to get right count in header button

--- src/components/Header/HeaderButton.js
+++ src/components/Header/HeaderButton.js
@@ -1,9 +1,16 @@
+import {useContext} from "react";
+import CartContext from "../../store/use-context";
+
 import classes from "./HeaderButton.module.css";
 
 const HeaderButton = (props) => {
-    const count=2;
+    const cartCtx = useContext(CartContext);
+    
+    const count = cartCtx.items.reduce((count,item) => count + item.amount);
+
     return (
         <button onClick={props.showCart} className={classes.button}>
+            <span>🛒</span>
             <span className={classes.name}>Your Orders</span>
             <span className={classes.count}>{count}</span>
         </button>
